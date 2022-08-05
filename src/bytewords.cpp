1c09c7cf2a37cf60cdcb07f360249f2e0081b0b1
Lawrence Nahum <lawrence@greenaddress.it>
2022-08-05 13:26:37 +0200

    esp32-build: precompute data for crc32 and bytewords

--- src/bytewords.cpp
+++ src/bytewords.cpp
@@ -16,31 +16,59 @@ using namespace std;
 
 static const char* bytewords = "ableacidalsoapexaquaarchatomauntawayaxisbackbaldbarnbeltbetabiasbluebodybragbrewbulbbuzzcalmcashcatschefcityclawcodecolacookcostcruxcurlcuspcyandarkdatadaysdelidicedietdoordowndrawdropdrumdulldutyeacheasyechoedgeepicevenexamexiteyesfactfairfernfigsfilmfishfizzflapflewfluxfoxyfreefrogfuelfundgalagamegeargemsgiftgirlglowgoodgraygrimgurugushgyrohalfhanghardhawkheathelphighhillholyhopehornhutsicedideaidleinchinkyintoirisironitemjadejazzjoinjoltjowljudojugsjumpjunkjurykeepkenokeptkeyskickkilnkingkitekiwiknoblamblavalazyleaflegsliarlimplionlistlogoloudloveluaulucklungmainmanymathmazememomenumeowmildmintmissmonknailnavyneednewsnextnoonnotenumbobeyoboeomitonyxopenovalowlspaidpartpeckplaypluspoempoolposepuffpumapurrquadquizraceramprealredorichroadrockroofrubyruinrunsrustsafesagascarsetssilkskewslotsoapsolosongstubsurfswantacotasktaxitenttiedtimetinytoiltombtoystriptunatwinuglyundouniturgeuservastveryvetovialvibeviewvisavoidvowswallwandwarmwaspwavewaxywebswhatwhenwhizwolfworkyankyawnyellyogayurtzapszerozestzinczonezoom";
 
+
+static const int16_t _lookup[] = {
+    4, 14, 29, 37, -1, -1, 73, -1, 99, -1, -1, 128, -1, -1, -1, 177,
+    -1, -1, 194, 217, -1, 230, -1, -1, 248, -1, -1, 20, -1, -1, -1, -1,
+    -1, -1, -1, -1, 126, 127, -1, 160, -1, -1, -1, -1, 203, 214, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, 53, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 253, 1, 11,
+    -1, -1, -1, 72, 80, 88, 98, -1, -1, 137, 149, 155, -1, 168, 179, 186,
+    -1, 210, -1, 231, 234, -1, -1, -1, 0, 16, 28, 40, 52, 69, 74, 95,
+    100, 107, 124, 138, 145, 159, 162, 175, -1, 181, 193, 211, 222, 228, 237, -1,
+    -1, 254, -1, -1, 25, -1, -1, -1, -1, 86, -1, -1, -1, 130, -1, -1,
+    -1, 176, -1, 188, 204, -1, -1, -1, 243, -1, -1, -1, -1, 18, -1, -1,
+    -1, 70, -1, 87, -1, -1, 123, 141, -1, -1, -1, -1, -1, -1, 202, -1,
+    -1, -1, -1, -1, -1, -1, 5, -1, 23, -1, 49, 63, 84, 92, 101, -1,
+    -1, -1, 144, -1, -1, -1, -1, 185, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, 39, -1, -1, -1, -1, -1, -1, 125, -1, -1, -1, -1, -1,
+    -1, -1, -1, 208, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, 10, 30, 36, -1, -1, -1, 89, -1, 115, 121, 140,
+    152, -1, -1, 170, -1, 187, 197, 207, -1, -1, 244, -1, 245, -1, -1, -1,
+    33, 47, -1, 71, 78, 93, -1, 111, -1, -1, -1, 153, 166, 174, -1, 183,
+    -1, 213, -1, 227, 233, -1, 247, -1, 6, -1, 22, 46, 55, 62, 82, -1,
+    106, -1, -1, -1, -1, -1, -1, 173, -1, -1, -1, -1, -1, -1, 235, -1,
+    -1, 255, -1, 12, 35, 43, 54, 60, -1, 96, 105, 109, 122, 134, 142, 158,
+    165, -1, -1, 190, 205, 218, -1, -1, 241, -1, 246, -1, 2, -1, -1, -1,
+    51, -1, 85, -1, 103, 112, 118, 136, 146, -1, -1, -1, -1, 184, 201, 206,
+    220, 226, -1, -1, -1, 251, -1, -1, 34, 45, -1, 65, -1, 91, -1, 114,
+    117, 133, -1, -1, -1, -1, -1, 182, 200, 216, -1, -1, 236, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 42, -1, 59,
+    75, -1, -1, -1, -1, 132, -1, -1, -1, 178, -1, -1, 195, -1, 223, -1,
+    -1, -1, -1, -1, 9, 15, 24, 38, 57, 61, 76, 97, 104, 113, 120, 131,
+    151, 156, 167, 172, -1, 191, 196, 215, -1, 232, 239, -1, -1, 250, 7, 13,
+    31, 41, 56, 58, 77, 90, -1, 110, 119, 135, 150, 157, 163, 169, -1, 192,
+    199, 209, 221, 224, 240, -1, 249, 252, -1, -1, -1, -1, -1, -1, 83, -1,
+    -1, -1, -1, 139, 147, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 19, 27, 44,
+    -1, 66, 79, -1, -1, -1, -1, -1, 148, -1, -1, -1, -1, -1, 198, -1,
+    -1, 229, -1, -1, -1, -1, 3, -1, 32, -1, -1, 67, -1, -1, -1, -1,
+    -1, -1, -1, -1, 164, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    8, 17, 26, 48, 50, 68, 81, 94, 102, 116, -1, 129, 143, 154, 161, 171,
+    -1, 189, -1, 212, 219, 225, 238, -1, -1, -1, -1, 21, -1, -1, -1, 64,
+    -1, -1, -1, 108, -1, -1, -1, -1, -1, -1, 180, -1, -1, -1, -1, -1,
+    242, -1, -1, -1
+};
+
 uint8_t decode_word(const string& word, size_t word_len) {
     if(word.length() != word_len) {
         throw runtime_error("Invalid Bytewords.");
     }
 
-    static int16_t* array = NULL;
     const size_t dim = 26;
-
-    // Since the first and last letters of each Byteword are unique,
-    // we can use them as indexes into a two-dimensional lookup table.
-    // This table is generated lazily.
-    if(array == NULL) {
-        const size_t array_len = dim * dim;
-        array = (int16_t*)malloc(array_len * sizeof(int16_t));
-        for(size_t i = 0; i < array_len; i++) {
-            array[i] = -1;
-        }
-        for(size_t i = 0; i < 256; i++) {
-            const char* byteword = bytewords + i * 4;
-            size_t x = byteword[0] - 'a';
-            size_t y = byteword[3] - 'a';
-            size_t offset = y * dim + x;
-            array[offset] = i;
-        }
-    }
 
     // If the coordinates generated by the first and last letters are out of bounds,
     // or the lookup table contains -1 at the coordinates, then the word is not valid.
@@ -50,7 +78,7 @@ uint8_t decode_word(const string& word, size_t word_len) {
         throw runtime_error("Invalid Bytewords.");
     }
     size_t offset = y * dim + x;
-    int16_t value = array[offset];
+    int16_t value = _lookup[offset];
     if(value == -1) {
         throw runtime_error("Invalid Bytewords.");
     }
